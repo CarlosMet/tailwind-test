2659b9687b7c005649bb013ea1b3cf83f067e689
Carlos metaute hoyos <carlosmet9409@gmail.com>
2023-08-04 14:06:38 -0500

    1st

---
+++ app/page.js
@@ -0,0 +1,13 @@
+
+
+export default function Home() {
+  return (
+    <div className='text-7xl font-black text-blue-300 text-center relative w-full h-screen'>
+      <div className='absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2'>
+        <h1 className=''>
+        testing vercel deploy with tailwind
+        </h1>
+      </div>
+    </div>
+  )
+}
