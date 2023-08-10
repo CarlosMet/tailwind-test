a6d631a0f5f36e888ec2a6a489c0f9721e3927c5
Carlos metaute hoyos <carlosmet9409@gmail.com>
2023-08-10 15:32:28 -0500

    responsive hero

--- app/Components/Topbar.jsx
+++ app/Components/Topbar.jsx
@@ -6,12 +6,12 @@ import {AiOutlineGithub, AiFillLinkedin} from 'react-icons/ai'
 
 export default function Topbar() {
   return (
-    <div className='flex justify-between items-center px-3 md:px-12 lg:px-16 xl:px-36 2xl:px-64 py-3 lg:py-5 xl:py-6 2xl:py-8 fixed top-0 w-full left-0'>
+    <div className='flex flex-col lg:flex-row justify-between items-center px-3 md:px-12 lg:px-16 xl:px-36 2xl:px-64 py-3 lg:py-5 xl:py-6 2xl:py-8 fixed top-0 w-full left-0'>
     <div>
         <p className='font-bold text-2xl lg:text-3xl xl:text-4xl'>Carlos Metaute.</p>
     </div>
-    <div>
-        <ul className='flex justify-between items-center gap-4'>
+    <div className='mt-3'>
+        <ul className='flex justify-center lg:justify-between items-center gap-4'>
           <li><AiOutlineGithub size={27}></AiOutlineGithub></li>
           <li><AiFillLinkedin size={27}></AiFillLinkedin></li>
           <li></li>
